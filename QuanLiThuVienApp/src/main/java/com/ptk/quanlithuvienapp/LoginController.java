a6d7e8cad51e10332c617930471035c1f01e932f
phamtrungkienit02 <2051052067kien@ou.edu.vn>
2022-12-01 21:02:39 +0700

    LoginForm

--- QuanLiThuVienApp/src/main/java/com/ptk/quanlithuvienapp/LoginController.java
+++ QuanLiThuVienApp/src/main/java/com/ptk/quanlithuvienapp/LoginController.java
@@ -10,9 +10,11 @@ import com.ptk.services.MessageLogin;
 import com.ptk.services.UserDao;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
@@ -21,23 +23,30 @@ import javafx.scene.paint.Color;
 public class LoginController implements Initializable {
     @FXML private TextField txtUser;
     @FXML private PasswordField txtPass;
+    @FXML private Label lbError;
+  
   
     @FXML private void switchToLogin() throws IOException {
         StringBuilder sb = new StringBuilder();
-        String err1 = "Tên đăng nhập không được để trống hoặc có khoảng trắng";
-        String err2 = "Mật khẩu không được để trống hoặc có khoảng trắng";
-        String err3 = "Tên đăng nhập hay mật khẩu sai";
-        String err4 = "Mật khẩu tối đa 9 kí tự";
+        String err1 = "username không được để trống hoặc có khoảng trắng";
+        String err2 = "password không được để trống hoặc có khoảng trắng";
+        String err3 = "username hay password sai";
+        String err4 = "password tối đa 9 kí tự";
+        String err5 = "Không nhập kí tự đặc biệt";
         
         DataValidator.validateEmpty(txtUser, err1, sb);
         DataValidator.passEmpty(txtPass, err2, sb);
         DataValidator.checkLength(txtPass, err4, sb);
+        DataValidator.checkSpecialChar(txtUser, txtPass, err5, sb);
         //co thong bao
         if (sb.length() > 0){
             MessageLogin.showErrorLogin("ERROR", sb.toString());
+            lbError.setTextFill(Color.RED);
+            lbError.setText(sb.toString());
             return;
         }
-        //else App.setRoot("primary");
+        else 
+            lbError.setText("");
        
         UserDao dao = new UserDao();
         try {
@@ -46,8 +55,8 @@ public class LoginController implements Initializable {
                 MessageLogin.showErrorLogin("ERROR", err3);
             }
             else{
-                //MessageLogin.showIndex("Đăng nhập thành công", "WELCOME TO LIBRARY MANAGER ");
                 App.setRoot("primary");
+                MessageLogin.showIndex("Đăng nhập thành công", "WELCOME TO LIBRARY MANAGER ");
             }
                 
         } catch(Exception e){
--- QuanLiThuVienApp/src/main/java/com/ptk/services/DataValidator.java
+++ QuanLiThuVienApp/src/main/java/com/ptk/services/DataValidator.java
@@ -43,4 +43,25 @@ public class DataValidator {
            field.requestFocus();
        }
     }
+    
+    public static void checkSpecialChar(TextField field1,PasswordField field2, String errorMessage, StringBuilder sb){
+        String u = field1.getText();
+        String p = field2.getText();
+        for(int i = 0; i < u.length(); i++){
+            if ((int)u.charAt(i) < 48 || ((int)u.charAt(i) > 57 && (int)u.charAt(i) < 65) ||
+                ((int)u.charAt(i) > 90 && (int)u.charAt(i) < 97) ||
+                  (int)u.charAt(i) > 123){
+                sb.append(errorMessage).append("\n");          
+                return;
+            }
+        }
+        for(int i = 0; i < p.length(); i++){
+            if ((int)p.charAt(i) < 48 || ((int)p.charAt(i) > 57 && (int)p.charAt(i) < 65) ||
+                ((int)p.charAt(i) > 90 && (int)p.charAt(i) < 97) ||
+                  (int)p.charAt(i) > 123){
+                sb.append(errorMessage).append("\n");          
+                return;
+            }
+        }
+    }
 }
